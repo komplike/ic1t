0d8082290db7e377fca3fa4891d9508fd37142f9
Martin Pecar <martin.pecar23@gmail.com>
2021-04-17 01:24:21 +0200

    Fixed testcalc.c

--- testcalc.c
+++ testcalc.c
@@ -5,17 +5,29 @@
 char *gets(char *);
 
 int main() {
-	struct {
-		char str[100];
-		double num[];
-	} op;
-
+	
 	const int NUMBER_OF_OPERANDS = 5;
-	char delim[] = "+-*/";
 
-	gets(op.str);
-	char *ptr = strtok(op.str, delim);
+	struct {
+		char input[100];
+		double numbers[NUMBER_OF_OPERANDS];
+		char operators[NUMBER_OF_OPERANDS - 1];
+	} formula;
 
+	char delimiters[] = "+-*/";
+	char numbers[] = "0123456789";
+	char copy[100];
+
+	//ACQUIRES INPUT FROM USER
+
+	gets(formula.input);
+	//strcpy(formula.input, "45+6-7*89/10"); //ONLY USED FOR DEBUGGING WITH DDD
+	
+	//MAKES A COPY TO ACQUIRE OPERATORS LATER
+	strcpy(copy, formula.input);
+
+	//READS NUMBERS FROM formula.input AND PUTS THEM TO formula.numbers
+	char *ptr = strtok(formula.input, delimiters);
 	int i = 0;
 	while(ptr != NULL)
 	{
@@ -23,18 +35,32 @@ int main() {
 		{
 			break;
 		}
-
-		//printf("%s\n", ptr);
-		op.num[i] = atof(ptr);
-		//printf("%f\n", op.num[i]);
-		ptr = strtok(NULL, delim);
+		formula.numbers[i] = atof(ptr);
+		printf("%f\n", formula.numbers[i]);
+		ptr = strtok(NULL, delimiters);
 		i++;
 	}
 
-	for (int j = 0; j<NUMBER_OF_OPERANDS;j++)
+	//READS OPERATOR SYMBOLS FROM copy AND PUTS THEM TO formula.operators
+	ptr = strtok(copy, numbers);
+	i = 0;
+	while(ptr != NULL)
 	{
-		printf("%f\n", op.num[j]);
+		formula.operators[i] = *ptr;
+		printf("%c\n", formula.operators[i]);
+		ptr = strtok(NULL, numbers);
+		i++;
 	}
+
+	//ACTUAL CALCULATOR NOW
+
+	//NUMBERS ARE STORED IN formula.nubers AND OPERATORS ARE STORED IN formula.operators
+
+	//just add some logic to make it work now. 2moro...
+
+
+
+
 
 	exit(0);
 }
