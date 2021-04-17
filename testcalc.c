e0480c6f13eeaf58f754db6255c2b191bc64d7c2
komplike <romansan.tn@gmail.com>
2021-04-17 14:30:58 +0200

    Add files via upload

--- testcalc.c
+++ testcalc.c
@@ -4,11 +4,32 @@
 
 char *gets(char *);
 
-int main() {
-	
+// debugging function to see results after each opperation
+void printStatus(double *numbers, int *ops, int op_left)
+{
+	while (op_left != 0)
+	{
+		for (int i = 0; i < op_left+1; i++)
+		{
+			printf("%lf ",numbers[i]);
+		}
+		printf("\n");
+		for (int i = 0; i < op_left; i++)
+		{
+			printf("%d ",ops[i]);
+		}
+		printf("\n");
+		op_left--;
+	}
+}
+
+int main()
+{
+
 	const int NUMBER_OF_OPERANDS = 5;
 
-	struct {
+	struct
+	{
 		char input[100];
 		double numbers[NUMBER_OF_OPERANDS];
 		char operators[NUMBER_OF_OPERANDS - 1];
@@ -22,21 +43,21 @@ int main() {
 
 	gets(formula.input);
 	//strcpy(formula.input, "45+6-7*89/10"); //ONLY USED FOR DEBUGGING WITH DDD
-	
+
 	//MAKES A COPY TO ACQUIRE OPERATORS LATER
 	strcpy(copy, formula.input);
 
 	//READS NUMBERS FROM formula.input AND PUTS THEM TO formula.numbers
 	char *ptr = strtok(formula.input, delimiters);
 	int i = 0;
-	while(ptr != NULL)
+	while (ptr != NULL)
 	{
 		if (i >= NUMBER_OF_OPERANDS)
 		{
 			break;
 		}
 		formula.numbers[i] = atof(ptr);
-		printf("%f\n", formula.numbers[i]);
+		// printf("%f\n", formula.numbers[i]);
 		ptr = strtok(NULL, delimiters);
 		i++;
 	}
@@ -44,23 +65,123 @@ int main() {
 	//READS OPERATOR SYMBOLS FROM copy AND PUTS THEM TO formula.operators
 	ptr = strtok(copy, numbers);
 	i = 0;
-	while(ptr != NULL)
+	while (ptr != NULL)
 	{
 		formula.operators[i] = *ptr;
-		printf("%c\n", formula.operators[i]);
+		// printf("%c\n", formula.operators[i]);
 		ptr = strtok(NULL, numbers);
 		i++;
 	}
 
 	//ACTUAL CALCULATOR NOW
 
-	//NUMBERS ARE STORED IN formula.nubers AND OPERATORS ARE STORED IN formula.operators
+	//NUMBERS ARE STORED IN formula.numbers AND OPERATORS ARE STORED IN formula.operators
 
-	//just add some logic to make it work now. 2moro...
+	//represent operands as numbers
+	int op[NUMBER_OF_OPERANDS - 1];
+	for (int i = 0; i < NUMBER_OF_OPERANDS - 1; i++)
+	{
+		switch (formula.operators[i])
+		{
+		case '*':
+			op[i] = 0;
+			break;
+		case '/':
+			op[i] = 1;
+			break;
+		case '+':
+			op[i] = 2;
+			break;
+		case '-':
+			op[i] = 3;
+			break;
+		default:
+			break;
+		}
+	}
 
+	// solve expression by operand priority
+	int op_left = NUMBER_OF_OPERANDS - 1;
+	while (op_left != 0)
+	{
+		//operation priority * /
+		for (int i = 0; i < NUMBER_OF_OPERANDS - 1; i++)
+		{
+			if (op[i] == 0)
+			{
+				op_left -= 1;
+				//do *
+				formula.numbers[i] = formula.numbers[i] * formula.numbers[i+1];
+				//shift left
+				for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
+				{
+					formula.numbers[j] = formula.numbers[j+1];
+				}
+				for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
+				{
+					op[j] = op[j+1];
+				}
+				i -= 1;
+				// printStatus(formula.numbers, op, op_left);
+			}
+			else if (op[i] == 1)
+			{
+				op_left -= 1;
+				//do /
+				formula.numbers[i] = formula.numbers[i] / formula.numbers[i+1];
+				//shift left
+				for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
+				{
+					formula.numbers[j] = formula.numbers[j+1];
+				}
+				for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
+				{
+					op[j] = op[j+1];
+				}
+				i -= 1;
+				// printStatus(formula.numbers, op, op_left);
+			}
+		}
+		//operation priority + -
+		for (int i = 0; i < NUMBER_OF_OPERANDS - 1; i++)
+		{
+			if (op[i] == 2)
+			{
+				op_left -= 1;
+				//do +
+				formula.numbers[i] = formula.numbers[i] + formula.numbers[i+1];
+				//shift left
+				for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
+				{
+					formula.numbers[j] = formula.numbers[j+1];
+				}
+				for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
+				{
+					op[j] = op[j+1];
+				}
+				i -= 1;
+				// printStatus(formula.numbers, op, op_left);
+			}
+			else if (op[i] == 3)
+			{
+				op_left -= 1;
+				//do -
+				formula.numbers[i] = formula.numbers[i] - formula.numbers[i+1];
+				//shift left
+				for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
+				{
+					formula.numbers[j] = formula.numbers[j+1];
+				}
+				for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
+				{
+					op[j] = op[j+1];
+				}
+				i -= 1;
+				// printStatus(formula.numbers, op, op_left);
+			}
+		}
+	}
+	printf("= %lf",formula.numbers[0]);
 
-
-
-
-	exit(0);
+	return (0);
 }
