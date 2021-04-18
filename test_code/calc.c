0673ea54104ea163d8f70a99116a7fbce4f047d2
komplike <romansan.tn@gmail.com>
2021-04-18 16:28:41 +0200

    update filesystem

---
+++ test_code/calc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char op;
+    double num1, num2, res = 0;
+    printf("Solve:\n");
+    while(1)
+    {
+    scanf("%lf",&num1);
+    scanf("%c",&op);
+    scanf("%lf",&num2);
+    switch ((int)op)
+    {
+        case 43:
+            res = num1 + num2;
+            break;
+        case 45:
+            res = num1 - num2;
+            break;
+        case 42:
+            res = num1 * num2;
+            break;
+        case 47:
+            if (num2 == 0) printf("Zero division!\n");
+            res = num1 / num2;
+            break;
+        default:
+            printf("Wrong operation!");
+            break;
+        }
+        
+    printf("= %lf\n",res);
+    }
+    return 0;
+}
