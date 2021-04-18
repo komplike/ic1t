5a16e4a360dfd598fb52dcbf3739c1f8db30841c
Martin Pecar <martin.pecar23@gmail.com>
2021-04-18 02:03:24 +0200

    Added better input, general bugfixes

--- testcalc.c
+++ testcalc.c
@@ -27,161 +27,217 @@ int main()
 {
 
 	const int NUMBER_OF_OPERANDS = 5;
+	const int MAX_INPUT = 50;
 
 	struct
 	{
-		char input[100];
+		char input[50];
+		volatile char memory[50];
 		double numbers[NUMBER_OF_OPERANDS];
 		char operators[NUMBER_OF_OPERANDS - 1];
 	} formula;
 
-	char delimiters[] = "+-*/";
-	char numbers[] = "0123456789.";
-	char copy[100];
 
-	//ACQUIRES INPUT FROM USER
-
-	gets(formula.input);
-	//strcpy(formula.input, "45+6-7*89/10"); //ONLY USED FOR DEBUGGING WITH DDD
-
-	//MAKES A COPY TO ACQUIRE OPERATORS LATER
-	strcpy(copy, formula.input);
-
-	//READS NUMBERS FROM formula.input AND PUTS THEM TO formula.numbers
-	char *ptr = strtok(formula.input, delimiters);
-	int i = 0;
-	while (ptr != NULL)
+	while(1)
 	{
-		if (i >= NUMBER_OF_OPERANDS)
+		//initialize to be empty
+		memset(formula.input, 0, 50*(sizeof formula.input[0]));
+		gets(formula.input);
+		//strcpy(formula.input, "15*0"); //only here for ddd debugging
+
+		//if you type in mem, you get the last result
+		if(!strcmp(formula.input, "mem"))
 		{
-			break;
+			printf("memory: %s\n", formula.memory);
+			continue;
 		}
-		formula.numbers[i] = atof(ptr);
-		// printf("%f\n", formula.numbers[i]);
-		ptr = strtok(NULL, delimiters);
-		i++;
-	}
 
-	//READS OPERATOR SYMBOLS FROM copy AND PUTS THEM TO formula.operators
-	ptr = strtok(copy, numbers);
-	i = 0;
-	while (ptr != NULL)
-	{
-		formula.operators[i] = *ptr;
-		// printf("%c\n", formula.operators[i]);
-		ptr = strtok(NULL, numbers);
-		i++;
-	}
-	int TRUE_NUM_OF_OPS = i;
-	//ACTUAL CALCULATOR NOW
+		//initialize to be empty
+		memset(formula.operators, 0, (NUMBER_OF_OPERANDS-1)*(sizeof formula.operators[0]));
 
-	//NUMBERS ARE STORED IN formula.numbers AND OPERATORS ARE STORED IN formula.operators
+		int position = 0;
+		int formula_position = 0;
+		char read[100] = "";
 
-	//represent operands as numbers
-	int op[TRUE_NUM_OF_OPS];
-	for (int i = 0; i < TRUE_NUM_OF_OPS; i++)
-	{
-		switch (formula.operators[i])
+		//goes through the entire input and splits it into numbers and operands
+		for (int i = 1; i < MAX_INPUT; i++) //OD 1 PROTOZE RADEK MUZE ZACINAT minus
 		{
-		case '*':
-			op[i] = 0;
-			break;
-		case '/':
-			op[i] = 1;
-			break;
-		case '+':
-			op[i] = 2;
-			break;
-		case '-':
-			op[i] = 3;
-			break;
-		default:
-			break;
-		}
-	}
-
-	// solve expression by operand priority
-	int op_left = TRUE_NUM_OF_OPS;
-	while (op_left != 0)
-	{
-		//operation priority * /
-		for (int i = 0; i < NUMBER_OF_OPERANDS - 1; i++)
-		{
-			if (op[i] == 0)
+			switch ((int)formula.input[i])
 			{
-				op_left -= 1;
-				//do *
-				formula.numbers[i] = formula.numbers[i] * formula.numbers[i+1];
-				//shift left
-				for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
-				{
-					formula.numbers[j] = formula.numbers[j+1];
-				}
-				for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
-				{
-					op[j] = op[j+1];
-				}
-				i -= 1;
-				// printStatus(formula.numbers, op, op_left);
+				case 43:
+				case 45:
+				case 42:
+				case 47:
+					//if last char is symbol, skips
+					if (i == MAX_INPUT -1)
+					{
+						break;
+					}
+
+					memset(read, 0, 50*(sizeof read[0]));
+					//copies (i - position) chars from formula.input from position formula.input[position]
+					memcpy(&read, &formula.input[position], (i - position)*sizeof(char));
+					formula.numbers[formula_position] = atof(read);
+
+					memset(read, 0, 50*(sizeof read[0]));
+					//adds the opertor to formula.operators same as above
+					memcpy(&read, &formula.input[i], sizeof(char));
+					formula.operators[formula_position] = (char)read[0];
+				
+					//printf("%f\n", formula.numbers[formula_position]);	//used for debugging
+					//printf("%c\n", formula.operators[formula_position]);	//used for debugging
+
+					//iterates position in array
+					formula_position++;
+					//iterates i because next symbol could be "-" as to not read it as an operator
+					i++;
+					position = i;
+
+					break;
 			}
-			else if (op[i] == 1)
+
+			//last number
+			if (i == (MAX_INPUT -1))
 			{
-				op_left -= 1;
-				//do /
-				formula.numbers[i] = formula.numbers[i] / formula.numbers[i+1];
-				//shift left
-				for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
+				if ((i - position) == 0)
 				{
-					formula.numbers[j] = formula.numbers[j+1];
+					memcpy(&read, &formula.input[position], sizeof(char));
+					formula.numbers[formula_position] = atof(read);     //od pos1 do i-1
+					//printf("%f\n", formula.numbers[formula_position]);
 				}
-				for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
+				else
 				{
-					op[j] = op[j+1];
+					memcpy(&read, &formula.input[position], (i - position)*sizeof(char));
+					formula.numbers[formula_position] = atof(read);     //od pos1 do i-1
+					//printf("%f\n", formula.numbers[formula_position]);
 				}
-				i -= 1;
-				// printStatus(formula.numbers, op, op_left);
 			}
 		}
-		//operation priority + -
-		for (int i = 0; i < NUMBER_OF_OPERANDS - 1; i++)
+
+		//numbers are stored in formula.numbers and operators are stored in formula.operators
+		int TRUE_NUM_OF_OPS = formula_position;
+
+		//represent operands as numbers
+		int op[TRUE_NUM_OF_OPS];
+		for (int i = 0; i < TRUE_NUM_OF_OPS; i++)
 		{
-			if (op[i] == 2)
+			switch (formula.operators[i])
 			{
-				op_left -= 1;
-				//do +
-				formula.numbers[i] = formula.numbers[i] + formula.numbers[i+1];
-				//shift left
-				for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
-				{
-					formula.numbers[j] = formula.numbers[j+1];
-				}
-				for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
-				{
-					op[j] = op[j+1];
-				}
-				i -= 1;
-				// printStatus(formula.numbers, op, op_left);
-			}
-			else if (op[i] == 3)
-			{
-				op_left -= 1;
-				//do -
-				formula.numbers[i] = formula.numbers[i] - formula.numbers[i+1];
-				//shift left
-				for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
-				{
-					formula.numbers[j] = formula.numbers[j+1];
-				}
-				for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
-				{
-					op[j] = op[j+1];
-				}
-				i -= 1;
-				// printStatus(formula.numbers, op, op_left);
+				case '*':
+					op[i] = 0;
+					break;
+				case '/':
+					op[i] = 1;
+					break;
+				case '+':
+					op[i] = 2;
+					break;
+				case '-':
+					op[i] = 3;
+					break;
+				default:
+					break;
 			}
 		}
+
+		// solve expression by operand priority
+		int op_left = TRUE_NUM_OF_OPS;
+		while (op_left != 0)
+		{
+			//operation priority * /
+			for (int i = 0; i < TRUE_NUM_OF_OPS; i++)
+			{
+				if (op[i] == 0)
+				{
+					//when a*b was entered, last for loop in this if clause caused the calculation to continue indefinitely, this prevents it
+					if(op_left == 0)
+					{
+						break;
+					}
+					op_left -= 1;
+					//do *
+					formula.numbers[i] = formula.numbers[i] * formula.numbers[i+1];
+					//shift left
+					for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
+					{
+						formula.numbers[j] = formula.numbers[j+1];
+					}
+					for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
+					{
+						op[j] = op[j+1];
+					}
+					i -= 1;
+					// printStatus(formula.numbers, op, op_left);
+				}
+				else if (op[i] == 1)
+				{
+					op_left -= 1;
+					//do /
+					if (formula.numbers[i+1] == 0)
+					{
+						printf("%s\n", "Division by zero!");
+						break;
+					}
+					formula.numbers[i] = formula.numbers[i] / formula.numbers[i+1];
+					//shift left
+					for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
+					{
+						formula.numbers[j] = formula.numbers[j+1];
+					}
+					for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
+					{
+						op[j] = op[j+1];
+					}
+					i -= 1;
+					// printStatus(formula.numbers, op, op_left);
+				}
+			}
+			//operation priority + -
+			for (int i = 0; i < TRUE_NUM_OF_OPS; i++)
+			{
+				if (op[i] == 2)
+				{
+					op_left -= 1;
+					//do +
+					formula.numbers[i] = formula.numbers[i] + formula.numbers[i+1];
+					//shift left
+					for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
+					{
+						formula.numbers[j] = formula.numbers[j+1];
+					}
+					for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
+					{
+						op[j] = op[j+1];
+					}
+					i -= 1;
+					// printStatus(formula.numbers, op, op_left);
+				}
+				else if (op[i] == 3)
+				{
+					op_left -= 1;
+					//do -
+					formula.numbers[i] = formula.numbers[i] - formula.numbers[i+1];
+					//shift left
+					for (int j = i+1; j < NUMBER_OF_OPERANDS; j++)
+					{
+						formula.numbers[j] = formula.numbers[j+1];
+					}
+					for (int j = i; j < NUMBER_OF_OPERANDS-1; j++)
+					{
+						op[j] = op[j+1];
+					}
+					i -= 1;
+					// printStatus(formula.numbers, op, op_left);
+				}
+			}
+		}
+
+		char mem[50];
+		snprintf(mem, 50, "%lf", formula.numbers[0]);
+		strcpy(formula.memory, mem);
+		//printf("mem = %s\n", formula.memory); //for debugging
+		printf("= %lf\n",formula.numbers[0]);
 	}
-	printf("= %lf\n",formula.numbers[0]);
 
 	return (0);
 }
